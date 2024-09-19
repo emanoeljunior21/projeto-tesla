ed7f9671e8b9d69d81833c17e943d03664e0ebd1
emanoeljunior21 <emanoeljuniordev@gmail.com>
2024-09-19 02:23:52 -0300

    ´projeto-tesla

---
+++ script.js
@@ -0,0 +1,9 @@
+let formulario = document.querySelector(".formulario");
+
+function aparecerFormulario() {
+  formulario.classList.add("visivel");
+}
+
+function desaparecerFormulario() {
+  formulario.classList.remove("visivel");
+}
